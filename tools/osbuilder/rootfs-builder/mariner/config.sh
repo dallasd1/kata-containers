4f21bb5e45da625e2e5963c01467cdcb9b92faf3
Dallas Delaney <dadelan@microsoft.com>
2023-03-13 12:32:13 -0700

    remove KATA_BUILD_CC

--- tools/osbuilder/rootfs-builder/mariner/config.sh
+++ tools/osbuilder/rootfs-builder/mariner/config.sh
@@ -7,5 +7,4 @@ OS_VERSION=${OS_VERSION:-2.0}
 LIBC="gnu"
 PACKAGES="chrony iptables kmod core-packages-base-image kernel ca-certificates openssl curl"
 [ "$AGENT_INIT" = no ] && PACKAGES+=" systemd"
-[ "$KATA_BUILD_CC" = yes ] && PACKAGES+=" cryptsetup-bin e2fsprogs"
 [ "$SECCOMP" = yes ] && PACKAGES+=" libseccomp"
